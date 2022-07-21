b2519f6d13feedb7645cae15ba1455622608b02a
Shmupoo <92347054+Shmupoo@users.noreply.github.com>
2022-07-21 09:53:17 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+function start() {
+    navigator.mediaDevices.getUserMedia({
+        audio: true
+    });
+
+    classifier = ml5.soundClassifier('https://teachablemachine.withgoogle.com/models/s9yNeITOK/model.json', modelReady);
+
+}
+
+function modelReady() {
+    classifier.classify(gotResults);
+}
+
+function gotResults(error, results) {
+    console.log('Got Results!!')
+}
